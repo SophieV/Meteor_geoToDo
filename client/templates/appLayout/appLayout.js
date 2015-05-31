4773b5d7962e1367b9313c3de2fd2c4d2d24ef23
Sophie Filipov <sophiefilipov@Sophies-MacBook-Pro.local>
2015-05-31 17:46:55 -0400

    routes using iron router added - no data context yet

--- client/templates/appLayout/appLayout.js
+++ client/templates/appLayout/appLayout.js
@@ -1,13 +1,13 @@
 // defined by folder
 //if (Meteor.isClient) {
 
-  Meteor.startup(function() {
-    GoogleMaps.load();
-  });
+  // Meteor.startup(function() {
+  //   GoogleMaps.load();
+  // });
 
   Meteor.subscribe("tasks");
 
-  Template.body.helpers({
+  Template.appLayout.helpers({
     incompleteCount: function () {
       return Tasks.find({checked: {$ne: true}}).count();
     }
