d4188034a561dc495cc9d76b356c58d025eece4d
Sophie Filipov <sophiefilipov@Sophies-MacBook-Pro.local>
2015-06-02 21:17:31 -0400

    navbar: highlighted menu option

--- client/templates/navbar/navbar.js
+++ client/templates/navbar/navbar.js
@@ -1,5 +1,6 @@
-  Template.task.events({
-    "click .nav": function () {
-    debugger;
+  Template.navbar.events({
+    "click .nav": function (event) {
+    	$(event.target).parent().parent().find('.active').removeClass('active').css('font-weight', 'normal');
+    	$(event.target).parent().addClass('active').css('font-weight', 'bold');
     }
   });
