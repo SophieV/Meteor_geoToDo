1c857e50667dd9afd58c78d2ae42bbeb9f7c7dbb
Sophie Filipov <sophiefilipov@Sophies-MacBook-Pro.local>
2015-06-05 10:48:04 -0400

    added missing file from previous commit - fixed requesting coordinates

---
+++ server/methods.js
@@ -0,0 +1,66 @@
+Meteor.methods({
+  getSum: function(num1, num2){
+    return num1+num2;
+  },
+  getGeoCoordinates: function (addressText) {
+    var url = ["http://maps.google.com/maps/api/geocode/json?address=", addressText].join('');
+    var result = HTTP.get(url, {timeout:30000});
+
+    if(result.statusCode==200) {
+      var response = JSON.parse(result.content);
+      console.log("response received.");
+      console.log(response);
+
+      if (response != null && response.results != null && response.results.length === 1)
+      {
+        if (response.results[0].geometry != null)
+        {
+          console.log(response.results[0].geometry.location);
+          return coordinates = response.results[0].geometry.location;
+        }
+      }
+    } else {
+      console.log("Response issue: ", result.statusCode);
+      var errorJson = JSON.parse(result.content);
+      throw new Meteor.Error(result.statusCode, errorJson.error);
+    }
+  },
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+  }
+});
